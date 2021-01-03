0ce3bb2c6a2503ac9864e04200d5f0c0cc11b529
akcgjc007 <akcgjc007@gmail.com>
2021-01-03 09:33:13 +0530

    rpcEndpoint auto connected.

--- web3.js
+++ web3.js
@@ -1,11 +1,5 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const seedPhrase = 'narrow detail raw leaf cat nest dose surprise rapid rebuild ticket only';
-const rpcEndPoint = 'HTTP://127.0.0.1:7545';
-
-const provider = new HDWalletProvider(seedPhrase, rpcEndPoint);
-
 const Web3 = require('web3');
-const web3 = new Web3(provider);
-
+const rpcEndPoint = 'HTTP://127.0.0.1:7545';
+const web3 = new Web3(rpcEndPoint);
 
 module.exports = web3
